252bc2c2f6f66458647e8d5d1fef19a79f88cf70
caglaozbb <caglaozbb@gmail.com>
2024-06-28 16:03:32 +0300

    fix: share on x

--- src/app/results/page.jsx
+++ src/app/results/page.jsx
@@ -10,6 +10,9 @@ import Link from 'next/link';
 function Results() {
     const answers = useQuestionStore((state) => state.answers);
     const questions = useQuestionStore((state) => state.questions);
+    const inputValue = useQuestionStore((state) => state.inputValue)
+
+    console.log(inputValue);
 
     const calculateScore = () => {
         let score = 0;
@@ -30,18 +33,8 @@ function Results() {
                 <Link href="//"><Button variant='resultsNav'>Home</Button></Link>
                 {/* <Link href="//"><Button variant='resultsNav'>Generate PDF</Button></Link>
                 <Link href="//"> <Button variant='resultsNav'>Generate Cheatsheet</Button></Link> */}
-                <a target='_blank' href={`http://twitter.com/share?text=i solved a quiz from quizcraft and this is my score ${calculateScore()} / ${questions.length}` + '. ' + 'https://quizcraft.quest/'}><Button variant='resultsNav'>Share Score</Button></a>
+                <a target='_blank' href={`http://twitter.com/share?text=i solved a '${inputValue}' quiz from quizcraft and this is my score ${calculateScore()} / ${questions.length}` + '. ' + 'https://quizcraft.quest/'}><Button variant='resultsNav'>Share Score</Button></a>
             </div>
-
-            {/* <ul>
-                {questions.map((question, index) => (
-                    <li key={index}>
-                        <div>Question: {question.text}</div>
-                        <div>Your answer: {question.options[answers[index]]}</div>
-                        <div>Correct answer: {question.options[question.answer]}</div>
-                    </li>
-                ))}
-            </ul> */}
         </div>
     );
 }
