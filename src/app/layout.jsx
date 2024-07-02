b1056e090f6950a8f4bf6e94cf57afbd3565cdef
caglaozbb <caglaozbb@gmail.com>
2024-07-02 17:56:24 +0300

    update:meta tag

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -28,6 +28,13 @@ export default function RootLayout({ children }) {
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="Quizcraft" />
+        <meta property="og:description" content="A Fun and Magical Learning Adventure" />
+        <meta property="og:image" content="/boiler.png" />
+        <meta property="og:url" content="https://quizcraft.quest/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en_US" />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <html lang="en" className={roboto.className}>
         <body>
