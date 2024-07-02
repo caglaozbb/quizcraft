272e08202425bec7135e96e38486e8af128ee8fe
caglaozbb <caglaozbb@gmail.com>
2024-07-02 18:06:57 +0300

    fix: meta

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -28,13 +28,12 @@ export default function RootLayout({ children }) {
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:title" content="Quizcraft" />
-        <meta property="og:description" content="A Fun and Magical Learning Adventure" />
-        <meta property="og:image" content="https://www.quizcraft.quest/_next/image?url=%2Fwitch-hat.png&w=256&q=75" />
-        <meta property="og:url" content="https://quizcraft.quest/" />
-        <meta property="og:type" content="website" />
-        <meta property="og:locale" content="en_US" />
-        <meta name="twitter:card" content="summary_large_image" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content="Quizcraft" />
+        <meta property="twitter:description" content="A Fun and Magical Learning Adventure" />
+        <meta property="twitter:image" content="https://www.quizcraft.quest/_next/image?url=%2Fwitch-hat.png&w=256&q=75" />
+
       </Head>
       <html lang="en" className={roboto.className}>
         <body>
